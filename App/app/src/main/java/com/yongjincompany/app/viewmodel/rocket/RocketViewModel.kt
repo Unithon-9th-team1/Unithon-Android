9ff2077a1518d132148ac6de53c75dd3b932c964
Leesin0222 <dragon02229@naver.com>
2022-09-04 03:34:26 +0900

    🐛 :: 잘못된 Response 변경

--- App/app/src/main/java/com/yongjincompany/app/viewmodel/rocket/RocketViewModel.kt
+++ App/app/src/main/java/com/yongjincompany/app/viewmodel/rocket/RocketViewModel.kt
@@ -3,13 +3,13 @@ package com.yongjincompany.app.viewmodel.rocket
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.yongjincompany.app.data.remote.response.memo.FetchMemoListResponse
+import com.yongjincompany.app.data.remote.response.rocket.FetchRocketListResponse
 import com.yongjincompany.app.repository.RocketRepository
 import kotlinx.coroutines.launch
 import retrofit2.Response
 
 class RocketViewModel(private val rocketRepository: RocketRepository): ViewModel() {
-    var fetchRocketListLiveData: MutableLiveData<Response<FetchMemoListResponse>> = MutableLiveData()
+    var fetchRocketListLiveData: MutableLiveData<Response<FetchRocketListResponse>> = MutableLiveData()
 
     fun fetchRocketList(nickname: String) {
         viewModelScope.launch {
