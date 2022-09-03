9b007c5bda1dc84ffbf97ae105115b385e21efca
Leesin0222 <dragon02229@naver.com>
2022-09-04 04:43:55 +0900

    ⚡ :: 로켓 탑승자 조회 api 작성

--- App/app/src/main/java/com/yongjincompany/app/data/remote/api/RocketApi.kt
+++ App/app/src/main/java/com/yongjincompany/app/data/remote/api/RocketApi.kt
@@ -2,14 +2,12 @@ package com.yongjincompany.app.data.remote.api
 
 import com.yongjincompany.app.data.remote.request.rocket.ReservationRocketRequest
 import com.yongjincompany.app.data.remote.request.rocket.RideRocketRequest
+import com.yongjincompany.app.data.remote.response.rocket.FetchRocketDriverResponse
 import com.yongjincompany.app.data.remote.response.rocket.FetchRocketListResponse
 import com.yongjincompany.app.data.remote.response.rocket.ReservationRocketResponse
 import com.yongjincompany.app.data.remote.response.rocket.RideRocketResponse
 import retrofit2.Response
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface RocketApi {
     @GET("rocket")
@@ -24,6 +22,11 @@ interface RocketApi {
 
     @POST("rocket-boarding")
     suspend fun rideRocket(
-        @Body rideRocketRequest: RideRocketRequest
+        @Body rideRocketRequest: RideRocketRequest,
     ): Response<RideRocketResponse>
+
+    @GET("rocket/{rocketId}")
+    suspend fun fetchRocketDriver(
+        @Path("rocketId") rocketId: Long,
+    ): Response<FetchRocketDriverResponse>
 }
