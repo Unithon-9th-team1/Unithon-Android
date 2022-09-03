533c73f110ca2b16c7b4ae02768a2f0c55c1452e
Leesin0222 <dragon02229@naver.com>
2022-09-04 04:01:02 +0900

    ⚡ :: 로켓 타기 뷰모델 작성

--- App/app/src/main/java/com/yongjincompany/app/viewmodel/rocket/RocketViewModel.kt
+++ App/app/src/main/java/com/yongjincompany/app/viewmodel/rocket/RocketViewModel.kt
@@ -4,8 +4,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.yongjincompany.app.data.remote.request.rocket.ReservationRocketRequest
+import com.yongjincompany.app.data.remote.request.rocket.RideRocketRequest
 import com.yongjincompany.app.data.remote.response.rocket.FetchRocketListResponse
 import com.yongjincompany.app.data.remote.response.rocket.ReservationRocketResponse
+import com.yongjincompany.app.data.remote.response.rocket.RideRocketResponse
 import com.yongjincompany.app.repository.RocketRepository
 import kotlinx.coroutines.launch
 import retrofit2.Response
@@ -13,6 +15,7 @@ import retrofit2.Response
 class RocketViewModel(private val rocketRepository: RocketRepository): ViewModel() {
     var fetchRocketListLiveData: MutableLiveData<Response<FetchRocketListResponse>> = MutableLiveData()
     var reservationRocketLiveData: MutableLiveData<Response<ReservationRocketResponse>> = MutableLiveData()
+    var rideRocketLiveData: MutableLiveData<Response<RideRocketResponse>> = MutableLiveData()
 
     fun fetchRocketList(nickname: String) {
         viewModelScope.launch {
@@ -27,4 +30,11 @@ class RocketViewModel(private val rocketRepository: RocketRepository): ViewModel
            reservationRocketLiveData.value = response
        }
    }
+
+    fun rideRocket(rideRocketRequest: RideRocketRequest) {
+        viewModelScope.launch {
+            val response = rocketRepository.rideRocket(rideRocketRequest)
+            rideRocketLiveData.value = response
+        }
+    }
 }
