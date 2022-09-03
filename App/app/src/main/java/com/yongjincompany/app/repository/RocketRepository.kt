11b35ec551982c47405ecfa99824115939552d76
Leesin0222 <dragon02229@naver.com>
2022-09-04 03:50:08 +0900

    ⚡ :: 로켓 예약하기 repository 코드 작성

--- App/app/src/main/java/com/yongjincompany/app/repository/RocketRepository.kt
+++ App/app/src/main/java/com/yongjincompany/app/repository/RocketRepository.kt
@@ -2,12 +2,18 @@ package com.yongjincompany.app.repository
 
 import com.yongjincompany.app.data.remote.RetrofitBuilder
 import com.yongjincompany.app.data.remote.request.memo.MemoInfoRequest
+import com.yongjincompany.app.data.remote.request.rocket.ReservationRocketRequest
 import com.yongjincompany.app.data.remote.response.memo.CreateMemoResponse
 import com.yongjincompany.app.data.remote.response.rocket.FetchRocketListResponse
+import com.yongjincompany.app.data.remote.response.rocket.ReservationRocketResponse
 import retrofit2.Response
 
 class RocketRepository {
     suspend fun fetchRocketList(nickname: String): Response<FetchRocketListResponse> {
         return  RetrofitBuilder.rocketApi.fetchRocketList(nickname)
+    }
+
+    suspend fun reservationRocket(reservationRocketRequest: ReservationRocketRequest): Response<ReservationRocketResponse> {
+        return RetrofitBuilder.rocketApi.reservationRocket(reservationRocketRequest)
     }
 }
