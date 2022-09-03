f9a687d873c4b50c21abd8f6230dfc38dcc90387
Leesin0222 <dragon02229@naver.com>
2022-09-04 03:49:55 +0900

    ⚡ :: 로켓 예약하기 api 작성

--- App/app/src/main/java/com/yongjincompany/app/data/remote/api/RocketApi.kt
+++ App/app/src/main/java/com/yongjincompany/app/data/remote/api/RocketApi.kt
@@ -1,8 +1,12 @@
 package com.yongjincompany.app.data.remote.api
 
+import com.yongjincompany.app.data.remote.request.rocket.ReservationRocketRequest
 import com.yongjincompany.app.data.remote.response.rocket.FetchRocketListResponse
+import com.yongjincompany.app.data.remote.response.rocket.ReservationRocketResponse
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface RocketApi {
@@ -10,4 +14,9 @@ interface RocketApi {
     suspend fun fetchRocketList(
         @Query("nickname") nickname: String,
     ): Response<FetchRocketListResponse>
+
+    @POST("rocket")
+    suspend fun reservationRocket(
+        @Body reservationRocketRequest: ReservationRocketRequest
+    ): Response<ReservationRocketResponse>
 }
