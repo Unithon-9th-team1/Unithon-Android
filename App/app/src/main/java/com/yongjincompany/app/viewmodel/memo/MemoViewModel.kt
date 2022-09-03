37517552486a1bebad394bf7a7bb9cb24620e3d0
Leesin0222 <dragon02229@naver.com>
2022-09-04 03:14:06 +0900

    ⚡ :: 랜덤메모필터링 viewmodel 작성

--- App/app/src/main/java/com/yongjincompany/app/viewmodel/memo/MemoViewModel.kt
+++ App/app/src/main/java/com/yongjincompany/app/viewmodel/memo/MemoViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.viewModelScope
 import com.yongjincompany.app.data.remote.request.memo.MemoInfoRequest
 import com.yongjincompany.app.data.remote.response.memo.CreateMemoResponse
 import com.yongjincompany.app.data.remote.response.memo.FetchMemoListResponse
+import com.yongjincompany.app.data.remote.response.memo.FilterRandomMemoResponse
 import com.yongjincompany.app.repository.MemoRepository
 import kotlinx.coroutines.launch
 import retrofit2.Response
@@ -14,6 +15,7 @@ import retrofit2.Response
 class MemoViewModel(private val memoRepository: MemoRepository) : ViewModel() {
     var createMemoLiveData: MutableLiveData<Response<CreateMemoResponse>> = MutableLiveData()
     var fetchMemoListLiveData: MutableLiveData<Response<FetchMemoListResponse>> = MutableLiveData()
+    var filterRandomMemoLiveData: MutableLiveData<Response<FilterRandomMemoResponse>> = MutableLiveData()
 
     fun createMemo(request: MemoInfoRequest) {
         viewModelScope.launch {
@@ -26,6 +28,13 @@ class MemoViewModel(private val memoRepository: MemoRepository) : ViewModel() {
         viewModelScope.launch {
             val response = memoRepository.fetchMemo(rocket, random)
             fetchMemoListLiveData.value = response
+        }
+    }
+
+    fun filterRandomMemo(rocket: String, random: Boolean) {
+        viewModelScope.launch {
+            val response = memoRepository.filterRandomMemo(rocket, random)
+            filterRandomMemoLiveData.value = response
         }
     }
 }
