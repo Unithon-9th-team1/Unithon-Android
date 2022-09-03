50df22cedfd385cad1ddd309173239082e74aa31
Leesin0222 <dragon02229@naver.com>
2022-09-04 03:50:32 +0900

    ⚡ :: 로켓 예약하기 뷰모델 작성

--- App/app/src/main/java/com/yongjincompany/app/viewmodel/rocket/RocketViewModel.kt
+++ App/app/src/main/java/com/yongjincompany/app/viewmodel/rocket/RocketViewModel.kt
@@ -3,13 +3,16 @@ package com.yongjincompany.app.viewmodel.rocket
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.yongjincompany.app.data.remote.request.rocket.ReservationRocketRequest
 import com.yongjincompany.app.data.remote.response.rocket.FetchRocketListResponse
+import com.yongjincompany.app.data.remote.response.rocket.ReservationRocketResponse
 import com.yongjincompany.app.repository.RocketRepository
 import kotlinx.coroutines.launch
 import retrofit2.Response
 
 class RocketViewModel(private val rocketRepository: RocketRepository): ViewModel() {
     var fetchRocketListLiveData: MutableLiveData<Response<FetchRocketListResponse>> = MutableLiveData()
+    var reservationRocketLiveData: MutableLiveData<Response<ReservationRocketResponse>> = MutableLiveData()
 
     fun fetchRocketList(nickname: String) {
         viewModelScope.launch {
@@ -17,4 +20,11 @@ class RocketViewModel(private val rocketRepository: RocketRepository): ViewModel
             fetchRocketListLiveData.value = response
         }
     }
+
+   fun reservationRocket(reservationRocketRequest: ReservationRocketRequest) {
+       viewModelScope.launch {
+           val response = rocketRepository.reservationRocket(reservationRocketRequest)
+           reservationRocketLiveData.value = response
+       }
+   }
 }
