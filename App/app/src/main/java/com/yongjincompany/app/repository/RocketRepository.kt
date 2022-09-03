057985b4d456658775978252f58f538da310d644
Leesin0222 <dragon02229@naver.com>
2022-09-04 04:00:42 +0900

    ⚡ ::  rocketRepository 작성

--- App/app/src/main/java/com/yongjincompany/app/repository/RocketRepository.kt
+++ App/app/src/main/java/com/yongjincompany/app/repository/RocketRepository.kt
@@ -3,9 +3,11 @@ package com.yongjincompany.app.repository
 import com.yongjincompany.app.data.remote.RetrofitBuilder
 import com.yongjincompany.app.data.remote.request.memo.MemoInfoRequest
 import com.yongjincompany.app.data.remote.request.rocket.ReservationRocketRequest
+import com.yongjincompany.app.data.remote.request.rocket.RideRocketRequest
 import com.yongjincompany.app.data.remote.response.memo.CreateMemoResponse
 import com.yongjincompany.app.data.remote.response.rocket.FetchRocketListResponse
 import com.yongjincompany.app.data.remote.response.rocket.ReservationRocketResponse
+import com.yongjincompany.app.data.remote.response.rocket.RideRocketResponse
 import retrofit2.Response
 
 class RocketRepository {
@@ -15,5 +17,9 @@ class RocketRepository {
 
     suspend fun reservationRocket(reservationRocketRequest: ReservationRocketRequest): Response<ReservationRocketResponse> {
         return RetrofitBuilder.rocketApi.reservationRocket(reservationRocketRequest)
+    }
+
+    suspend fun rideRocket(rideRocketRequest: RideRocketRequest): Response<RideRocketResponse> {
+        return RetrofitBuilder.rocketApi.rideRocket(rideRocketRequest)
     }
 }
