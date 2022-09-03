820585ae7b497542afdcd9f49a3eba7b7432d7af
최우성 <vov1252@naver.com>
2022-09-03 19:57:41 +0900

    ⚡ ::  다이얼로그 생성

--- App/app/src/main/java/com/yongjincompany/app/SpaceActivity.kt
+++ App/app/src/main/java/com/yongjincompany/app/SpaceActivity.kt
@@ -1,12 +1,14 @@
 package com.yongjincompany.app
 
+import com.bumptech.glide.Glide
 import com.semicolon.walkhub.ui.base.BaseActivity
-import com.yongjincompany.app.databinding.ActivityRideBinding
+import com.yongjincompany.app.databinding.ActivitySpaceBinding
 
-class SpaceActivity : BaseActivity<>(
-    R.layout.activity_ride
+class SpaceActivity : BaseActivity<ActivitySpaceBinding>(
+    R.layout.activity_space
 ) {
-
     override fun initView() {
+        Glide.with(this).load(R.drawable.space).centerCrop().into(binding.ivSpace)
+
     }
 }
